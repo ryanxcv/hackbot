df507be2ebfb859f5ae471392452f58faec0bad7
Ryan Burns <ryanxcv@gmail.com>
2015-07-13 01:56:45 -0700

    Updated ability framework

--- Ability.java
+++ Ability.java
@@ -1,27 +1,53 @@
 package hackbotcore;
 
+import java.util.Set;
+
+import hackbotutil.*;
+
 public abstract class Ability {
 
-    private static String name;
-    private static String desc;
-
-    private static Type type;
+    private final String name;
+    private final String desc;
+    private final Type type;
+    private final int range;
 
     public static enum Type { ATTACK, STATUS }
 
-    public abstract void use(Unit target);
+    public Ability(String name, String desc, Type type, int range) {
+        this.name = name;
+        this.desc = desc;
+        this.type = type;
+        this.range = range;
+    }
+    public Ability(String name, String desc, Type type) {
+        this.name = name;
+        this.desc = desc;
+        this.type = type;
+        this.range = 1;
+    }
+    public Ability(String name, String desc, int range) {
+        this.name = name;
+        this.desc = desc;
+        this.type = Type.ATTACK;
+        this.range = range;
+    }
+    public Ability(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+        this.type = Type.ATTACK;
+        this.range = 1;
+    }
 
     public Type getType() { return type; }
 
+    public abstract void use(Unit target);
+
+    public Set<Coordinate> rangeSet(Coordinate coord) {
+        return coord.distanceSet(range);
+    }
+
     public static class Cut extends Ability {
-
-        private static String name = "Cut";
-        private static String desc = "A basic attack";
-
-        private static Type type = Type.ATTACK;
-
-        public void use(Unit target) {
-            return;
-        }
+        public Cut() { super("Cut", "A basic attack"); }
+        public void use(Unit target) { return; }
     }
 }
