9262df5c355bb6ff56c38996e1f8d2e9183ed8b3
Ryan Burns <ryanxcv@gmail.com>
2015-07-16 03:33:29 -0700

    Changed test game

--- TestGame.java
+++ TestGame.java
@@ -13,7 +13,9 @@ public class TestGame {
         Battle battle = new Battle(14, 12);
         GameInterface iface = new GameInterface(battle);
         battle.addPlayerUnit(new Hack(new Coordinate(1, 2)));
-        battle.addComputerUnit(new Sentinel(new Coordinate(6, 6)));
+        battle.addPlayerUnit(new Hack(new Coordinate(2, 1)));
+        battle.addComputerUnit(new Sentinel(new Coordinate(4, 5)));
+        battle.addComputerUnit(new Sentinel(new Coordinate(6, 4)));
 
         UI ui = new UI(iface);
     }
